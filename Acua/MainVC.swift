23d1810dd734bfdada2b1eefe57caf6f4657e777
AppHero2 <apphero.software@gmail.com>
2018-05-28 02:45:28 +1000

    build 13 - prototype

--- Acua/MainVC.swift
+++ Acua/MainVC.swift
@@ -18,35 +18,48 @@ class MainVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load each of the view controllers you want to embed
-        // from the storyboard.
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let bookingVC = storyboard.instantiateViewController(withIdentifier: "BOOKING")
-        let appointVC = storyboard.instantiateViewController(withIdentifier: "APPOINTMENT")
-        
-        // Initialize a FixedPagingViewController and pass
-        // in the view controllers.
-        let pagingViewController = FixedPagingViewController(viewControllers: [
-            bookingVC,
-            appointVC
-            ])
-        
-        // Make sure you add the PagingViewController as a child view
-        // controller and contrain it to the edges of the view.
-        addChildViewController(pagingViewController)
-        view.addSubview(pagingViewController.view)
-        view.constrainToEdges(pagingViewController.view)
-        pagingViewController.didMove(toParentViewController: self)
-        
         let user = AppManager.shared.getUser()
+        
         if user != nil {
             AppManager.shared.startTrackingOrders()
             AppManager.shared.startTrackingUser(userId: user!.idx!)
             AppManager.shared.startTrackingNotification(uid: user!.idx!)
+         
+            AppManager.shared.sideMenuDelegate = self
+            AppManager.shared.userStatusDelegate = self
+            
+            if user!.userType == 0 {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let bookingVC = storyboard.instantiateViewController(withIdentifier: "BOOKING")
+                let appointVC = storyboard.instantiateViewController(withIdentifier: "APPOINTMENT")
+                let pagingViewController = FixedPagingViewController(viewControllers: [
+                    bookingVC,
+                    appointVC
+                    ])
+                
+                // Make sure you add the PagingViewController as a child view
+                // controller and contrain it to the edges of the view.
+                addChildViewController(pagingViewController)
+                view.addSubview(pagingViewController.view)
+                view.constrainToEdges(pagingViewController.view)
+                pagingViewController.didMove(toParentViewController: self)
+            } else {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let statisticVC = storyboard.instantiateViewController(withIdentifier: "STATISTIC")
+                let appointVC = storyboard.instantiateViewController(withIdentifier: "APPOINTMENT")
+                let pagingViewController = FixedPagingViewController(viewControllers: [
+                    statisticVC,
+                    appointVC
+                    ])
+                
+                // Make sure you add the PagingViewController as a child view
+                // controller and contrain it to the edges of the view.
+                addChildViewController(pagingViewController)
+                view.addSubview(pagingViewController.view)
+                view.constrainToEdges(pagingViewController.view)
+                pagingViewController.didMove(toParentViewController: self)
+            }
         }
-        
-        AppManager.shared.sideMenuDelegate = self
-        AppManager.shared.userStatusDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
